01bebea9b7683a30970162ad7e2059a6897681fb
Michael Blaß <michael.blass@uni-hamburg.de>
2019-01-08 16:08:00 +0100

    Refactoring stats

--- hmm/stats.c
+++ hmm/stats.c
@@ -2,24 +2,24 @@
 
 
 scalar
-poisson_log_pmf(scalar lambda, long x)
+poisson_log_pmf (scalar lambda, long x)
 {
-    return (scalar) x * logl(lambda) - lgamma((scalar) x + 1) - lambda;
+    return (scalar) x * logl (lambda) - lgamma ((scalar) x + 1) - lambda;
 }
 
 
 scalar
-poisson_pmf(scalar lambda, long x)
+poisson_pmf (scalar lambda, long x)
 {
-    return expl(poisson_log_pmf(lambda, x));
+    return expl (poisson_log_pmf (lambda, x));
 }
 
 
 void
-ppmf(scalar *lambda, size_t m,  long x, scalar *out)
+ppmf (scalar *lambda, size_t m,  long x, scalar *out)
 {
     for (size_t i = 0; i < m; i++)
     {
-        out[i] = poisson_pmf( lambda[i], x );
+        out[i] = poisson_pmf (lambda[i], x);
     }
 }
