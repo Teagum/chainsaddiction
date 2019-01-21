d6e0e7d48c6221f8bc7c02d3996b011e74cec6c1
Michael Blaß <michael.blass@uni-hamburg.de>
2019-01-21 06:27:29 +0100

    add new dataset and parameter files

--- tests/src/test_fwbw.c
+++ tests/src/test_fwbw.c
@@ -3,24 +3,25 @@
 #include <stdlib.h>
 #include "fwbw.h"
 #include "utilities.h"
+#include "hmm.h"
 
 int main(int argc, char *argv[])
 {
-    if (argc != 3)
+    if (argc != 4)
     {
-        fprintf (stdout, "Usage: test_fwbw -[abp] m < dataset\n");
+        fprintf (stdout, "Usage: test_fwbw -[abp] m param_file < dataset\n");
         return 0;
     }
 
     size_t m = atoi (argv[2]);
     scalar *output = NULL;
 
-
-    scalar  l[] = { 10L, 20L, 30L };
-    scalar  g[] = { .8, .1, .1,
-                    .1, .8, .1,
-                    .1, .1, .8 };
-    scalar  d[] = { 1.0/3.0, 1.0/3.0, 1.0/3.0 };
+    PoisParams *params = PoisHmm_ParamsFromFile(argv[3]);
+    if (params == NULL)
+    {
+        fprintf (stderr, "Error reading parameter file.\n");
+        return  1;
+    }
 
     DataSet *X = read_dataset();
     if (X == NULL)
@@ -34,7 +35,8 @@ int main(int argc, char *argv[])
     scalar *probs = malloc (X->size * m * sizeof (scalar));
     if (alpha == NULL || beta == NULL || probs == NULL)
     {
-        free (alpha); free (beta); free (probs); return 0;
+        fprintf (stderr, "Allocation error.\n");
+        free (alpha); free (beta); free (probs); return 1;
     }
 
     if (strcmp(argv[1], "-a") == 0) output = alpha;
@@ -42,14 +44,13 @@ int main(int argc, char *argv[])
     else if (strcmp(argv[1], "-p") == 0) output = probs;
     else output = alpha;
 
-    log_poisson_forward_backward(X->data, X->size, m, l, g, d, alpha, beta, probs);
-
+    PoisHmm_log_forward_backward (X->data, X->size, m, params, alpha, beta, probs);
 
     for (size_t i = 0; i < X->size; i++)
     {
         for (size_t j = 0; j < m; j++)
         {
-            printf("%30.20Lf", output[i*m+j] );
+            printf("%40.30Lf", output[i*m+j] );
         }
         printf("\n");
     }
@@ -58,6 +59,7 @@ int main(int argc, char *argv[])
     free (beta);
     free (probs);
     free_dataset (X);
-    
+    PoisHmm_FreeParams (params);
+
     return 0;
 }
