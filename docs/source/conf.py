b0e0ceb6233c1ae00e31b6fd05326172e4db2f38
Michael Blaß <michael.blass@uni-hamburg.de>
2019-05-20 12:24:50 +0200

    Zid

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ author = 'Michael Blaß'
 
 # The full version, including alpha/beta/rc tags
 release = '0.1'
-
+master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------
 
