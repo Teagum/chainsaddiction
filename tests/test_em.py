c5ef634859b2c10efa5d7f38ec5455919cd92ae7
Michael Blaß <michael.blass@uni-hamburg.de>
2020-02-12 08:51:42 +0100

    Unfinished update.

--- tests/test_em.py
+++ tests/test_em.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 import unittest
 import numpy as np
 import chains_addiction as ca
@@ -5,6 +6,8 @@ import chains_addiction as ca
 """ test_em.py
 Test cases for EM algorithm.
 """
+
+Array = np.ndarray
 earthquake = np.array([  13, 14,  8, 10, 16, 26, 32, 27, 18, 32,
                 36, 24, 22, 23, 22, 18, 25, 21, 21, 14,
                  8, 11, 14, 23, 18, 17, 19, 20, 22, 19,
@@ -17,12 +20,52 @@ earthquake = np.array([  13, 14,  8, 10, 16, 26, 32, 27, 18, 32,
                 18, 16, 13, 12, 13, 20, 15, 16, 12, 18,
                 15, 16, 13, 15, 16, 11, 11])
 
+class HmmParams:
+    m_states: int
+    init_lambda: Array
+    init_gamma: Array
+    init_delta: Array
+    max_iter: int
+    tol: float
 
+class TestEM(unittest.TestCase):
+    def setUp(self):
+        self.params = HmmParams(3, np.array([10, 250, 450]),
+            np.array([[.8, .1, .1], [.1, .8, .1], [.1, .1, .8]]),
+
+
+        self.params_random = {
+            'x_train': np.random.randint(0, 500, 1000),
+            'm_states': 3,
+            'init_lambda': 
+            'init_gamma': 
+            'init_delta': np.array([.3, .3, .3]),
+            'max_iter': 1000,
+            'tol': 1e-6}
+
+        self.params_earthquake = {
+            'x_train': earthquake,
+            'm_states': 3,
+            'init_lambda': np.array([10., 20., 30.]),
+            'init_gamma': np.array([[.8, .1, .1], [.1, .8, .1], [.1, .1, .8]]),
+            'init_delta': np.array([1/3., 1/3., 1/3.]),
+            'max_iter': 1000,
+            'tol': 1e-6}
+
+    def test_random(self):
+        ph = PoissonHMM(**self.params_random)
+        self.assertFalse(np.allclose(ph.gamma_, gamma_))
+
+    def test_earthquake(self):
+        ph = PoissonHMM(**self.params_earthquake)
+        self.assertFalse(np.allclose(ph.gamma_, gamma_))
+
+"""
 class PoissonHMM:
-    def __init__(self, x_trains, m_states, init_lambda, init_gamma,
-            init_delta, max_iter, tol):
-        ret = ca.hmm_poisson_EM(x_train, init_lambda, init_gamma,
-                init_delta, max_iter, tol)
+    def __init__(self, x_train, m_states: int, init_lambda, init_gamma,
+            init_delta, max_iter: int, tol: float):
+        ret = ca.hmm_poisson_fit_em(x_train, m_states, init_lambda,
+                init_gamma, init_delta, max_iter, tol)
         self.success = ret[0]
         self.lambda_ = ret[1]
         self.gamma_ = ret[2]
@@ -37,32 +80,4 @@ class PoissonHMM:
             '{}\nBIC: {}\nnll: {}\nn_iter: {}\n')
         return fmt.format(self.success, self.lambda_, self.gamma_, self.delta_,
             self.aic, self.bic, self.nll, self.n_iter)
-
-class TestEM(unittest.TestCase):
-    def setUp(self):
-        self.params_random = {
-            'x_train': np.random.randint(0, 500, 1000),
-            'm_states': 3,
-            'init_lambda': np.array([10, 250, 450]),
-            'init_gamma': np.array([[.8, .1, .1], [.1, .8, .1], [.1, .1, .8]]),
-            'init_delta': np.array([.3, .3, .3]),
-            'max_iter': 1000,
-            'tol': 1e-6 }
-
-        self.params_earthquake = {
-            'x_train': earthquake,
-            'm_states': 3,
-            'init_lambda': np.array([10., 20., 30.]),
-            'init_gamma': np.array([[.8, .1, .1], [.1, .8, .1], [.1, .1, .8]]),
-            'init_delta': np.array([1/3., 1/3., 1/3.]),
-            'max_iter': 1000,
-            'tol': 1e-6 }
-
-    def test_random(self):
-        ph = PoissonHMM(*self.params_random)
-        self.assertFalse(np.allclose(ph.gamma_, gamma_))
-
-    def test_earthquake(self):
-        ph = PoissonHMM(*self.params_earthquake)
-        self.assertFalse(np.allclose(ph.gamma_, gamma_))
-
+"""
