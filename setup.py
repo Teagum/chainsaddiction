a249cb2eb5fff86f95b68dc91494bb40f58f3d35
Michael Blaß <michael.blass@uni-hamburg.de>
2020-06-08 11:52:43 +0200

    Usage of VLAs will cause compilation error.

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ config = read_configuration('setup.cfg')
 
 ext = Extension('chains_addiction',
         sources = [f'{srcf!s}' for srcf in src_path.glob('*.c')],
-        include_dirs = ['include/', np.get_include()])
+        include_dirs = ['include/', np.get_include()],
+        extra_compile_args = ['-Werror=vla'])
 
 setup(ext_modules = [ext])
