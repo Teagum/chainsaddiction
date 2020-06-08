c16b53620ac4107a61cba913427004c473a5e8ed
Michael Blaß <michael.blass@uni-hamburg.de>
2020-06-08 11:36:21 +0200

    Updated setup

--- setup.py
+++ setup.py
@@ -2,16 +2,13 @@
 from pathlib import Path
 from setuptools import setup, Extension
 from setuptools.config import read_configuration
-
-
-class GetNumpyInclude:
-    def __str__(self):
-        import numpy
-        return numpy.get_include()
+import numpy as np
 
 src_path = Path('chains_addiction')
 config = read_configuration('setup.cfg')
+
 ext = Extension('chains_addiction',
         sources = [f'{srcf!s}' for srcf in src_path.glob('*.c')],
-        include_dirs = ['include/', GetNumpyInclude()])
+        include_dirs = ['include/', np.get_include()])
+
 setup(ext_modules = [ext])
