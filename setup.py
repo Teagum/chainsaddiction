da5435e5c38b24183455f1c5f3a497259f23309d
Michael Blaß <michael.blass@uni-hamburg.de>
2020-06-11 09:01:39 +0200

    Deleted extra compile args due to incompatibility with MSVC

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ config = read_configuration('setup.cfg')
 ext = Extension('chains_addiction',
         sources = [f'{srcf!s}' for srcf in src_path.glob('*.c')],
         include_dirs = ['include/', np.get_include()],
-        extra_compile_args = ['-Werror=vla'])
+        # extra_compile_args = ['-Werror=vla'],    # not supported by MSVC
+        language = 'c')
 
 setup(ext_modules = [ext])
