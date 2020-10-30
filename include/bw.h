a9d61822b8af9ff929aea70ad8a1d1e8f8a72f42
Michael Blaß <michael.blass@uni-hamburg.de>
2020-10-30 11:50:15 +0100

    Baum Welch Algorithm.

---
+++ include/bw.h
@@ -0,0 +1,24 @@
+#ifndef BW_H
+#define BW_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include "restrict.h"
+#include "fwbw.h"
+#include "scalar.h"
+#include "hmm.h"
+#include "utilities.h"
+
+int
+ca_bw_pois_e_step ();
+
+int
+ca_bw_pois_m_step ();
+
+int
+ca_bw_pois (
+    const DataSet *restrict _inp,
+    PoisHmm *restrict _init);
+
+#endif  /* BW_H */
---
+++ src/chainsaddiction/bw.c
@@ -0,0 +1,17 @@
+#include <string.h>
+#include "em.h"
+#include "hmm.h"
+
+int
+ca_bw_pois_e_step ()
+{}
+
+int
+ca_bw_pois_m_step ()
+{}
+
+int
+ca_bw_pois (
+    const DataSet *restrict _inp,
+    PoisHmm *restrict _init)
+{}
