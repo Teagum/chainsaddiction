c0f530440e3d26457d4a01f3172e203c5aa9581c
Michael Blaß <michael.blass@uni-hamburg.de>
2021-02-17 17:01:36 +0100

    basic libma features.

---
+++ src/chainsaddiction/libma.c
@@ -0,0 +1,47 @@
+#include "libma.h"
+
+size_t
+Ma_TypeSize (enum ma_types type)
+{
+    size_t type_size = 0;
+    switch (type)
+    {
+        case MA_SHORT:
+            type_size = sizeof (short);
+            break;
+
+        case MA_INT:
+            type_size = sizeof (int);
+            break;
+
+        case MA_FLOAT:
+            type_size = sizeof (float);
+            break;
+
+        case MA_DOUBLE:
+            type_size = sizeof (double);
+            break;
+
+        case MA_LONG_DOUBLE:
+            type_size = sizeof (long double);
+            break;
+    }
+    return type_size;
+}
+
+
+void *
+Ma_ArrayMemAlloc (const size_t n_elem, enum ma_types type, bool init)
+{
+    size_t size = Ma_TypeSize (type);
+    void *mem = NULL;
+    if (init)
+    {
+        mem = calloc (n_elem, size);
+    } else {
+        mem = malloc (n_elem * size);
+    }
+    MA_ASSERT_ALLOC (mem, "Ma_ArrayMemAlloc: Could not allocate buffer.");
+    return mem;
+}
+
