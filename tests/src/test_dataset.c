7e27a556e5c9d6fc34e8d8f31f1cdf8141e2dedf
Michael Blaß <mblass@posteo.net>
2021-03-31 06:43:00 +0200

    Add tests for dataset.
    Add tests for `Ca_NewDataSet', `ds_set', and `ds_get'.

--- tests/src/test_dataset.c
+++ tests/src/test_dataset.c
@@ -8,6 +8,10 @@ int main (void)
     srand (time (NULL));
 
     FEEDBACK (test_Ca_NewDataSet);
+    FEEDBACK (test_ds_set_error_on_idx_out_of_bounds);
+    FEEDBACK (test_ds_set_values);
+    FEEDBACK (test_ds_get_error_on_idx_out_of_bounds);
+    FEEDBACK (test_ds_get_values);
 
     if (N_ERRORS == 0)
     {
@@ -26,9 +30,109 @@ bool
 test_Ca_NewDataSet (void)
 {
     scalar acc = 0;
+    bool cond = false;
+
     DataSet *inp = Ca_NewDataSet ();
     for (size_t i=0; i<inp->size; i++) {
         acc += inp->data[i];
     }
-    return ASSERT_EQUAL (0, acc);
+    cond = ASSERT_EQUAL (0, acc);
+    CA_FREE_DATASET (inp);
+    return cond;
 }
+
+
+
+
+bool
+test_ds_set_error_on_idx_out_of_bounds (void)
+{
+    enum { n_iter=1000, val=11 };
+    DataSet *inp = Ca_NewDataSet ();
+
+    for (size_t i=0; i<n_iter; i++)
+    {
+        size_t idx = (size_t) rnd_int (DATASET_INIT_SIZE, INT_MAX);
+
+        ds_set (inp, idx, val);
+        if (!inp->err) {
+            CA_FREE_DATASET (inp);
+            return false;
+        }
+    }
+
+    CA_FREE_DATASET (inp);
+    return true;
+}
+
+
+bool
+test_ds_set_values (void)
+{
+    enum { n_iter=1000 };
+    DataSet *inp = Ca_NewDataSet ();
+
+    for (size_t i=0; i<n_iter; i++)
+    {
+        size_t idx = (size_t) rnd_int (0, DATASET_INIT_SIZE);
+        scalar val = rnd ();
+
+        ds_set (inp, idx, val);
+        if (inp->err || !ASSERT_EQUAL (inp->data[idx], val)) {
+            CA_FREE_DATASET (inp);
+            return false;
+        }
+    }
+    CA_FREE_DATASET (inp);
+    return true;
+}
+
+
+bool
+test_ds_get_error_on_idx_out_of_bounds (void)
+{
+    enum { n_iter=1000 };
+    DataSet *inp = Ca_NewDataSet ();
+
+    for (size_t i=0; i<n_iter; i++)
+    {
+        scalar val = 0;
+        size_t idx = (size_t) rnd_int (DATASET_INIT_SIZE, INT_MAX);
+
+        ds_get (inp, idx, &val);
+        if (!inp->err) {
+            CA_FREE_DATASET (inp);
+            return false;
+        }
+    }
+
+    CA_FREE_DATASET (inp);
+    return true;
+}
+
+
+bool
+test_ds_get_values (void)
+{
+    enum { n_iter=1000 };
+    DataSet *inp = Ca_NewDataSet ();
+
+    for (size_t i=0; i<n_iter; i++)
+    {
+        scalar val = rnd ();
+        size_t idx = (size_t) rnd_int (0, DATASET_INIT_SIZE);
+        scalar out = 0;
+
+        ds_set (inp, idx, val);
+        ds_get (inp, idx, &out);
+        if (inp->err || !ASSERT_EQUAL (val, out)) {
+           CA_FREE_DATASET (inp);
+           return false;
+        }
+    }
+
+    CA_FREE_DATASET (inp);
+    return true;
+}
+
+
--- tests/src/test_dataset.h
+++ tests/src/test_dataset.h
@@ -1,10 +1,12 @@
 #ifndef TEST_DATASET_H
 #define TEST_DATASET_H
 
+#include <limits.h>
 #include <stdio.h>
 #include <time.h>
 #include "unittest.h"
 #include "scalar.h"
+#include "rnd.h"
 #include "restrict.h"
 #include "dataset.h"
 
@@ -12,5 +14,16 @@
 bool
 test_Ca_NewDataSet (void);
 
+bool
+test_ds_set_error_on_idx_out_of_bounds (void);
+
+bool
+test_ds_set_values (void);
+
+bool
+test_ds_get_error_on_idx_out_of_bounds (void);
+
+bool
+test_ds_get_values (void);
 
 #endif  /* TEST_DATASET_H */
