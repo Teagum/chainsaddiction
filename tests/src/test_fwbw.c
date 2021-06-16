8c16486b5111f373b1f7d2b23815029a2a705238
Michael Blaß <mblass@posteo.net>
2021-06-16 07:21:56 +0200

    Update forward-backward algorithm.
    Add const qualifiers to pointers that do not move.
    Set up basic test infrastructure.
    Rename `alpha' and `beta' variables to `lalpha' and `lbeta'
    respectively. This is because they always hold log values. Hence, the
    'l' stands for 'log'.
    Rename 'log_forward_backward' to 'log_fwbw'.
    Replace '@' with '\' in doc strings.

--- tests/src/test_fwbw.c
+++ tests/src/test_fwbw.c
@@ -7,29 +7,9 @@ main (void)
     SETUP;
 
     RUN_TEST (test_log_forward);
-    /*j
-    scalar *lpp = alloc_block (m_states * data->size);
-    scalar *alpha = alloc_block (m_states * data->size);
-    scalar *beta = alloc_block (m_states * data->size);
+    RUN_TEST (test_log_backward);
+    RUN_TEST (test_log_fwbw);
 
-    v_poisson_logpmf (data->data, data->size, lambda, m_states, lpp);
-    log_forward_backward (lpp, gamma, delta, m_states, data->size, alpha, beta);
-
-    for (size_t i = 0; i < data->size; i++)
-    {
-        fprintf (stdout, "[%4zu]\t", i);
-        for (size_t j = 0; j < m_states; j++)
-        {
-            fprintf (stdout, "%10.5Lf\t", alpha[i*m_states+j]);
-        }
-        fprintf (stdout, "|\t");
-        for (size_t j = 0; j < m_states; j++)
-        {
-            fprintf (stdout, "%10.5Lf\t", beta[i*m_states+j]);
-        }
-        fprintf (stdout, "\n");
-    }
-    */
     EVALUATE;
 }
 
@@ -43,14 +23,87 @@ test_log_forward (void)
                             .1, .1, .8};
     const scalar delta[] = {0.333333, 0.333333, .333333};
 
-    vi_log (gamma, m_states * m_states);
-    vi_log (delta, m_states);
+    scalar lgamma[9] = { 0. };
+    scalar ldelta[3] = { 0. };
 
-    puts ("\n");
-    for (size_t i = 0; i < m_states; i++) {
-        for (size_t j = 0; j < m_states; j++) {
-            printf ("%10.5Lf", gamma[i*m_states+j]);
-        }
-    }
-    return true;
+    DataSet *eq = ds_NewFromFile ("tests/data/earthquakes");
+    size_t n_probs = eq->size * m_states;
+    DataSet *lpp = ds_New (n_probs);
+    DataSet *lalpha = ds_New (n_probs);
+
+    v_log (gamma, m_states*m_states, lgamma);
+    v_log (delta, m_states, ldelta);
+
+    v_poisson_logpmf (eq->data, eq->size, lambda, m_states, lpp->data);
+
+    log_forward (lpp->data, lgamma, ldelta, m_states, eq->size,  lalpha->data);
+
+    ds_FREE (lalpha);
+    ds_FREE (lpp);
+    ds_FREE (eq);
+    return false;
+}
+
+
+bool
+test_log_backward (void)
+{
+    const size_t m_states = 3;
+    const scalar lambda[] = {10, 20, 30};
+    const scalar gamma[] = {.8, .1, .1,
+                            .1, .8, .1,
+                            .1, .1, .8};
+    const scalar delta[] = {0.333333, 0.333333, .333333};
+
+    scalar lgamma[9] = { 0. };
+    scalar ldelta[3] = { 0. };
+
+    DataSet *eq = ds_NewFromFile ("tests/data/earthquakes");
+    size_t n_probs = eq->size * m_states;
+    DataSet *lpp = ds_New (n_probs);
+    DataSet *lbeta = ds_New (n_probs);
+
+    v_log (gamma, m_states*m_states, lgamma);
+    v_log (delta, m_states, ldelta);
+
+    v_poisson_logpmf (eq->data, eq->size, lambda, m_states, lpp->data);
+    log_backward (lpp->data, lgamma, m_states, eq->size,  lbeta->data);
+
+    ds_FREE (lbeta);
+    ds_FREE (lpp);
+    ds_FREE (eq);
+    return false;
+}
+
+
+bool
+test_log_fwbw (void)
+{
+    const size_t m_states = 3;
+    const scalar lambda[] = {10, 20, 30};
+    const scalar gamma[] = {.8, .1, .1,
+                            .1, .8, .1,
+                            .1, .1, .8};
+    const scalar delta[] = {0.333333, 0.333333, .333333};
+
+    scalar lgamma[9] = { 0. };
+    scalar ldelta[3] = { 0. };
+
+    DataSet *eq = ds_NewFromFile ("tests/data/earthquakes");
+    size_t n_probs = eq->size * m_states;
+    DataSet *lpp = ds_New (n_probs);
+    DataSet *lalpha = ds_New (n_probs);
+    DataSet *lbeta = ds_New (n_probs);
+
+    v_log (gamma, m_states*m_states, lgamma);
+    v_log (delta, m_states, ldelta);
+
+    v_poisson_logpmf (eq->data, eq->size, lambda, m_states, lpp->data);
+    log_fwbw (lpp->data, lgamma, ldelta, m_states, eq->size, lalpha->data, lbeta->data);
+
+    ds_FREE (lalpha);
+    ds_FREE (lbeta);
+    ds_FREE (lpp);
+    ds_FREE (eq);
+    return false;
 }
--- tests/src/test_fwbw.h
+++ tests/src/test_fwbw.h
@@ -15,4 +15,7 @@ test_log_forward (void);
 bool
 test_log_backward (void);
 
+bool
+test_log_fwbw (void);
+
 #endif  /* TEST_FWBW_H */
