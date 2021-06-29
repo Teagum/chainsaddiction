fb3e0f7f8d107680befa7e4d6096c622b240dbe5
Michael Blaß <mblass@posteo.net>
2021-06-29 13:17:14 +0200

    Finish `test__ph_bw_m_step_lambda'.

--- tests/src/test_bw.c
+++ tests/src/test_bw.c
@@ -15,26 +15,32 @@ main (void)
 bool
 test__ph_bw_m_step_lambda (void)
 {
-    size_t m_states = 3;
+    enum { n_repeat_test = 100 };
 
-    DataSet *inp = ds_NewFromFile ("tests/data/earthquakes");
-    PoisHmm *phmm = PoisHmm_New (inp->size, m_states);
-    scalar *lstate_pr = MA_SCALAR_ZEROS (phmm->n_obs * phmm->m_states);
-    scalar *new_lambda = MA_SCALAR_ZEROS (phmm->m_states);
-
-    ca_ph_InitRandom (phmm);
-    phmm->llh = PoisHmm_LogLikelihood (phmm->probs->lalpha, inp->size, phmm->m_states);
-    PoisHmm_LogStateProbs (phmm->probs, phmm->llh, lstate_pr);
-    ph_bw_m_step_lambda (inp, lstate_pr, m_states, new_lambda);
-
-    puts ("");
-    for (size_t i = 0; i < m_states; i++)
+    for (size_t n = 0; n < n_repeat_test; n++)
     {
-        printf ("[%3zu] %10.5Lf %10.5Lf\n", i, phmm->init->lambda[i], new_lambda[i]);
+        size_t m_states = (size_t) rnd_int (1, 100);
+        DataSet *inp = ds_NewFromFile ("tests/data/earthquakes");
+        PoisHmm *phmm = PoisHmm_New (inp->size, m_states);
+        scalar *lstate_pr = MA_SCALAR_ZEROS (phmm->n_obs * phmm->m_states);
+        scalar *new_lambda = MA_SCALAR_ZEROS (phmm->m_states);
+
+        PoisHmm_InitRandom (phmm);
+        ph_bw_e_step (inp, phmm);
+        PoisHmm_LogStateProbs (phmm->probs, phmm->llh, lstate_pr);
+        ph_bw_m_step_lambda (inp, lstate_pr, m_states, new_lambda);
+
+        for (size_t i = 0; i < m_states; i++)
+        {
+            if (!isnormal (new_lambda[i]))
+            {
+                return true;
+            }
+        }
+        ds_FREE (inp);
+        PoisHmm_Delete (phmm);
+        MA_FREE (lstate_pr);
+        MA_FREE (new_lambda);
     }
-    ds_FREE (inp);
-    PoisHmm_Delete (phmm);
-    MA_FREE (lstate_pr);
-    MA_FREE (new_lambda);
     return false;
 }
