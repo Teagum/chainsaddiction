d1f155837825b2c8ec3edfd410d1220c2dfe0712
Michael Blaß <mblass@posteo.net>
2021-06-30 11:28:29 +0200

    Define `PoisProbs' API
    Change the name of the former `HmmProbs' struct to `PoisProbs' and add
    the field `lcxpct', which is an object pointer intended to keep track
    of the conditional expectations during the computation of E step of
    Baum-Welch-Algorithm.

---
+++ src/chainsaddiction/poishmm/pois_probs.c
@@ -0,0 +1,8 @@
+#include "pois_probs.h"
+
+
+PoisProbs *
+PoisProbs_New (
+    const scalar n_obs,
+    const scalar m_states)
+{}
---
+++ src/chainsaddiction/poishmm/pois_probs.h
@@ -0,0 +1,40 @@
+#ifndef pois_probs_h
+#define pois_probs_h
+
+#include <stdlib.h>
+#include "../restrict.h"
+#include "../scalar.h"
+
+
+/** Computation buffer for HMM estimation.
+ *
+ * Each field is a pointer to continuous memory with
+ * space for n_obs * m_states values.
+ */
+typedef struct {
+    size_t n_obs;
+    size_t m_states;
+    scalar *lsdp;       /**< Log of the state dependent probabilities   */
+    scalar *lalpha;     /**< Log forward probabilities                  */
+    scalar *lbeta;      /**< Log backward probabilities                 */
+    scalar *lcxpt;      /**< Log conditional expectations               */
+} PoisProbs;
+
+
+#define PoisProbs_Delete(probs) do {    \
+    free (probs->lsdp);                 \
+    free (probs->lalpha);               \
+    free (probs->lbeta);                \
+    free (probs->lcexpt);               \
+    free (probs)                        \
+    probs = NULL;                       \
+} while (false)
+
+
+PoisProbs *
+PoisProbs_New (
+    const scalar n_obs,
+    const scalar m_states);
+
+
+#endif  /* pois_probs_h */
