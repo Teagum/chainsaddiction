003449523dee812db6dd0415665a5eac938954bb
Michael Blaß <mblass@posteo.net>
2021-06-30 10:53:27 +0200

    Define `PoisParams' API

---
+++ src/chainsaddiction/poishmm/poisparams.c
@@ -0,0 +1,25 @@
+#include "poisparams.h"
+
+
+void
+PoisParams_New (
+    const size_t m_states)
+{}
+
+
+inline void
+PoisParams_SetLambda (
+    PoisParams *const restrict params)
+{}
+
+
+inline void
+PoisParams_SetGamma (
+    PoisParams *const restrict params)
+{}
+
+
+inline void
+PoisParams_SetDelta (
+    PoisParams *const restrict params)
+{}
---
+++ src/chainsaddiction/poishmm/poisparams.h
@@ -0,0 +1,46 @@
+#ifndef poisparams_h
+#define poisparams_h
+
+#include <stdlib.h>
+#include "../restrict.h"
+#include "../scalar.h"
+
+
+typedef struct {
+    size_t m_states;
+    scalar *restrict lambda;
+    scalar *restrict gamma;
+    scalar *restrict delta;
+} PoisParams;
+
+
+#define PoisParams_Delete(params) do {      \
+    free (params->lambda);                  \
+    free (params->gamma);                   \
+    free (params->delta);                   \
+    free (params);                          \
+    params = NULL;                          \
+} while (false)
+
+
+void
+PoisParams_New (
+    const size_t m_states);
+
+
+extern void
+PoisParams_SetLambda (
+    PoisParams *const restrict params);
+
+
+extern void
+PoisParams_SetGamma (
+    PoisParams *const restrict params);
+
+
+extern void
+PoisParams_SetDelta (
+    PoisParams *const restrict params);
+
+
+#endif  /* poisparams_h */
