2d4e8117ab435e44f96d646bd0114624628cd928
Michael Blaß <mblass@posteo.net>
2021-06-30 15:19:25 +0200

    Implementation of `PoisProbs_New'

--- src/chainsaddiction/poishmm/pois_probs.c
+++ src/chainsaddiction/poishmm/pois_probs.c
@@ -5,4 +5,36 @@ PoisProbs *
 PoisProbs_New (
     const scalar n_obs,
     const scalar m_states)
-{}
+{
+    size_t n_elem = n_obs * m_states;
+    if (n_obs == 0)
+    {
+        fprintf (stderr, "`n_obs' must be greater than 0.\n");
+        return NULL;
+    }
+    if (m_states == 0)
+    {
+        fprintf (stderr, "`m_states' must be greater than 0.\n");
+        return NULL;
+    }
+    if (n_elem / n_obs != m_states)
+    {
+        fprintf (stderr, "Integer overflow detected.");
+        return NULL;
+    }
+
+    PoisProbs *probs = malloc (sizeof *probs);
+    if (probs == NULL)
+    {
+        fprintf (stderr, "Could not allocate memory for PoisProbs object.\n");
+        return NULL;
+    }
+    probs->lsdp     = MA_SCALAR_ZEROS (n_elem);
+    probs->lalpha   = MA_SCALAR_ZEROS (n_elem);
+    probs->lbeta    = MA_SCALAR_ZEROS (n_elem);
+    probs->lcxpt    = MA_SCALAR_ZEROS (n_elem);
+    probs->n_obs    = n_obs;
+    probs->m_states = m_states;
+
+    return probs;
+}
