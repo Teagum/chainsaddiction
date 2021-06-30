14d8a350241962da109b6a9c0f99202692385f4d
Michael Blaß <mblass@posteo.net>
2021-06-30 14:42:57 +0200

    Fix includes

--- src/chainsaddiction/poishmm/pois_params.h
+++ src/chainsaddiction/poishmm/pois_params.h
@@ -1,8 +1,10 @@
 #ifndef pois_params_h
 #define pois_params_h
 
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "../libma.h"
 #include "../restrict.h"
 #include "../scalar.h"
 
