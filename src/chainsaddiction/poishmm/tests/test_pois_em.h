c6a082ca96272ab09813831f31f57bc5e14ea8ed
Michael Blaß <mblass@posteo.net>
2021-07-07 10:21:20 +0200

    Setup test data

--- src/chainsaddiction/poishmm/tests/test_pois_em.h
+++ src/chainsaddiction/poishmm/tests/test_pois_em.h
@@ -4,6 +4,27 @@
 #include "unittest.h"
 #include "../pois_em.h"
 
+#define SET_EARTHQUAKES_SHORT const scalar input[] = \
+        { 13, 14, 8, 10, 16, 26, 32, 27, 18, 32, 36, 24, 22, 23, 22 }
+
+#define SET_LAMBDA const scalar lambda[] = { 10, 20, 30 }
+#define SET_DELTA  const scalar delta[]  = { .5, .3, .2 }
+#define SET_GAMMA  const scalar gamma[]  = { .7, .2, .1,      \
+                                             .1, .7, .2,      \
+                                             .2, .1, .7 }
+
+
+#define SET_LOG_DELTA                   \
+    SET_DELTA;                          \
+    scalar ldelta[m_states];            \
+    v_log (delta, m_states, ldelta)     \
+
+
+#define SET_LOG_GAMMA                           \
+    SET_GAMMA;                                  \
+    scalar lgamma[m_states*m_states];           \
+    v_log (gamma, m_states*m_states, lgamma)    \
+
 
 bool
 test__ph_bw_m_step_lambda (void);
