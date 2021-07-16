3916c48d1d4f7896fff74fbc8cc06fe7ee841d7c
Michael Blaß <mblass@posteo.net>
2021-07-16 09:19:09 +0200

    Remove legacy main methods

--- src/vmath/tests/test_vmath.c
+++ src/vmath/tests/test_vmath.c
@@ -1,26 +1,4 @@
 #include "test_vmath.h"
-#include <assert.h>
-
-
-int main (void)
-{
-    SETUP;
-    RUN_TEST (test_strided_max); //
-    RUN_TEST (test_v_lse);
-    RUN_TEST (test_vs_lse_centroid);
-    RUN_TEST (test_v_max);
-    RUN_TEST (test_vs_sum); //
-    RUN_TEST (test_m_max);
-    RUN_TEST (test_m_row_max); //
-
-    RUN_TEST (test_m_col_max);
-    RUN_TEST (test_log_vmp);   //
-
-    RUN_TEST (test_log_mvp);
-    RUN_TEST (test_m_lse_centroid_rows);
-
-    EVALUATE;
-}
 
 
 bool
