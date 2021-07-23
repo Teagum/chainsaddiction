76473007c95b21b352e5b5dfa87ed79afc835daf
Michael Blaß <mblass@posteo.net>
2021-07-23 13:00:34 +0200

    Type dependent printf formats
    Define printf formats in scalar.h that depend on wheterh ``LD_MATH`` is
    set or not. Define the following formats
    
    - SF    Singe scalar
    - SFN   Single scalar with newline
    - RSF   Read singel scalar with max. 40 digits

--- src/chainsaddiction/scalar.h
+++ src/chainsaddiction/scalar.h
@@ -7,4 +7,14 @@ typedef	long double	scalar;
 typedef double scalar;
 #endif
 
+#ifdef LD_MATH
+#define SF "%10.5Lf"
+#define SFN "%10.5Lf\n"
+#define RSF "%40Lf"
+#else
+#define SF "%10.5f"
+#define SFN "%10.5f\n"
+#define RSF "%40lf"
+#endif
+
 #endif	/* SCALAR_H */
