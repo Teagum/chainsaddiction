b3679ef7c42027cc0881839cc2971bc8721d7fce
Michael Blaß <mblass@posteo.net>
2021-07-31 13:04:34 +0200

    Add `PyArray_NEW_LD'
    This macro constructs a new Numpy array with elements of type long
    double and contiguous memory.

--- src/chainsaddiction/hmm_module.h
+++ src/chainsaddiction/hmm_module.h
@@ -19,6 +19,10 @@
                 PyArray_DescrFromType (NPY_LONGDOUBLE), \
                 2, shape, NULL, NULL, 0, NULL));
 
+
+#define PyArray_NEW_LD(py_obj) \
+    (PyArrayObject *) PyArray_FROM_OTF (py_obj, NPY_LONGDOUBLE, NPY_ARRAY_IN_ARRAY);
+
 /** Fit HMM for given parameters using EM. Return all params
  * and quality measures.
  */
