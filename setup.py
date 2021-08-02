1ca6f11650d64d0e100b406f1ca603899fa4f774
Michael Blaß <mblass@posteo.net>
2021-08-02 10:21:19 +0200

    Revert "Add import guard"
    Import guard is not compatible with tox.
    
    This reverts commit 09382a0a207f03fd45adfd6707b894042986631b.

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@
 
 import itertools
 from pathlib import Path
-import sys
 from typing import Generator, Tuple
 from setuptools import setup, Extension
 import numpy as np
@@ -19,36 +18,23 @@ def list_source_files (paths: Tuple[str, ...]) -> list:
     return list(itertools.chain.from_iterable(cglob(path) for path in paths))
 
 
-def main(argv=None) -> int:
-    """Setup entry point
+c_src_dirs = (
+    'src/vmath',
+    'src/chainsaddiction',
+    'src/chainsaddiction/poishmm',
+)
 
-    List all directory that include C source files in the appropriate tuples
-    below.
-    """
-    if argv is None:
-        argv = sys.argv
+c_include_dirs = (
+    'include',
+    'src/chainsaddiction/',
+    'src/chainsaddiction/poishmm',
+    np.get_include()
+)
 
-    c_src_dirs = (
-        'src/vmath',
-        'src/chainsaddiction',
-        'src/chainsaddiction/poishmm',
-    )
-
-    c_include_dirs = (
-        'include',
-        'src/chainsaddiction/',
-        'src/chainsaddiction/poishmm',
-        np.get_include()
-    )
-
-    ext = Extension('chainsaddiction',
+ext = Extension('chainsaddiction',
         sources = list_source_files(c_src_dirs),
         include_dirs = c_include_dirs,
         extra_compile_args = ['-Wall', '-Wextra'],
         language = 'c')
 
-    return setup(ext_modules = [ext])
-
-
-if __name__ == '__main__':
-    sys.exit(main())
+setup(ext_modules = [ext])
