72a5bbd7279d6ad341360edd356d70cb0b326b1d
Michael Blaß <mblass@posteo.net>
2021-09-01 13:52:53 +0200

    [Maint] Improve formatting

--- src/chainsaddiction/scalar.h
+++ src/chainsaddiction/scalar.h
@@ -2,19 +2,20 @@
 #define SCALAR_H
 
 #ifdef _NO_LD_MATH
-typedef double scalar;
+    typedef double scalar;
 #else
-typedef	long double	scalar;
+    typedef	long double	scalar;
 #endif
 
 #ifdef _NO_LD_MATH
-#define SF "%15.5f"
-#define SFN "%15.5f\n"
-#define RSF "%40lf"
+    #define SF "%15.5f"
+    #define SFN "%15.5f\n"
+    #define RSF "%40lf"
 #else
-#define SF "%15.5Lf"
-#define SFN "%15.5Lf\n"
-#define RSF "%40Lf"
+    #define SF "%15.5Lf"
+    #define SFN "%15.5Lf\n"
+    #define RSF "%40Lf"
 #endif
+
 
 #endif	/* SCALAR_H */
