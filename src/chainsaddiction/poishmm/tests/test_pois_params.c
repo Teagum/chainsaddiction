c008af94273e4889f0b5b57f593d674b187e52bc
Michael Blaß <mblass@posteo.net>
2021-09-07 10:43:55 +0200

    Fix `test__PoisParams_New`

--- src/chainsaddiction/poishmm/tests/test_pois_params.c
+++ src/chainsaddiction/poishmm/tests/test_pois_params.c
@@ -4,31 +4,27 @@
 bool
 test__PoisParams_New (void)
 {
-    enum { n_repeat_test = 10 };
+    const size_t m_states = rnd_int (1, 100);
+    PoisParams *params = PoisParams_New (m_states);
 
-    for (size_t n = 0; n < n_repeat_test; n++)
-    {
-        size_t m_states = (size_t) RAND_INT (1, 100);
-        PoisParams *params = PoisParams_New (m_states);
-
-        for (size_t i=0; i<m_states; i++) {
-            if (fpclassify (params->lambda[i]) != FP_ZERO ||
-                fpclassify (params->delta[i]) != FP_ZERO)
-            {
-                PoisParams_Delete (params);
-                return true;
-            }
+    for (size_t i=0; i<m_states; i++) {
+        if (fpclassify (params->lambda[i]) != FP_ZERO ||
+            fpclassify (params->delta[i]) != FP_ZERO)
+        {
+            PoisParams_Delete (params);
+            return true;
         }
-
-        for (size_t i=0; i<m_states*m_states; i++) {
-            if (fpclassify (params->gamma[i]) != FP_ZERO)
-            {
-                PoisParams_Delete (params);
-                return true;
-            }
-        }
-        PoisParams_Delete (params);
     }
+
+    for (size_t i=0; i<m_states*m_states; i++) {
+        if (fpclassify (params->gamma[i]) != FP_ZERO)
+        {
+            PoisParams_Delete (params);
+            return true;
+        }
+    }
+
+    PoisParams_Delete (params);
     return false;
 }
 
