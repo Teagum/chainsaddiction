b57ae37b2d8e9e6db9e915e7052b834e0bc62d5c
Michael Blaß <mblass@posteo.net>
2021-09-13 10:15:48 +0200

    Implemet simple module test structure

---
+++ tests/test-decoding.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+import sys
+import unittest
+
+import numpy as np
+from apollon.hmm import utilities as utils
+from chainsaddiction import poishmm
+
+
+class TestLocalDecoding(unittest.TestCase):
+    def setUp(self):
+        self.m_states = 3
+
+    def test_local_decoding(self):
+        pass
+
+
+class TestGloblaDecoding(unittest.TestCas):
+    def setUp(self):
+        self.m_states = 3
+
+    def test_global_decoding(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
