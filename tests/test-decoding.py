fa7a6e39b87bc930f595b99621dac6a36f21199d
Michael Blaß <mblass@posteo.net>
2021-09-13 16:48:53 +0200

    Update decoding test

--- tests/test-decoding.py
+++ tests/test-decoding.py
@@ -3,24 +3,32 @@ import sys
 import unittest
 
 import numpy as np
-from apollon.hmm import utilities as utils
+from apollon.hmm import utilities as hmmutils
 from chainsaddiction import poishmm
+
+import utils
 
 
 class TestLocalDecoding(unittest.TestCase):
     def setUp(self):
-        self.m_states = 3
+        self.lcxpt = np.fromfile('data/earthquakes/3s/lcxpt', dtype='float128')
+        self.res = np.fromfile('data/earthquakes/3s/locald', dtype='float128')
 
     def test_local_decoding(self):
-        pass
+        dec = poishmm.local_decoding(self.lcxpt)
+        self.assertEqual(dec, self.res)
 
 
-class TestGloblaDecoding(unittest.TestCas):
+class TestGloblaDecoding(unittest.TestCase):
     def setUp(self):
-        self.m_states = 3
+        self.lcxpt = np.fromfile('data/earthquakes/3s/lcxpt', dtype='float128')
+        self.lgamma = np.fromfile('data/earthquakes/3s/lgamma_', dtype='float128')
+        self.ldelta = np.fromfile('data/earthquakes/3s/ldelta_', dtype='float128')
+        self.res = np.formfile('data/earthquakes/3s/globald_', dtype='float128')
 
     def test_global_decoding(self):
-        pass
+        dec = poishmm.global_decoding(self.lgamma, self.ldelta, self.lcxpt)
+        self.assertEqual(dec, self.res)
 
 
 if __name__ == '__main__':
