a4c32e3adcb4c21198c2d5d09ae4e9726ded09a2
Michael Blaß <mblass@posteo.net>
2021-09-21 12:30:47 +0200

    Fix wrong test file names

--- tests/test-utils.py
+++ tests/test-utils.py
@@ -11,8 +11,8 @@ from chainsaddiction import poishmm
 class TestLocalDecoding(unittest.TestCase):
     def setUp(self):
         src = 'data/earthquakes/3s'
-        self.lcxpt = load_data(src, 'lcxpt').reshape(-1, 3)
-        self.res = load_data(src, 'locald', 'uint64')
+        self.lcxpt = load_data(src, 'lcsp.txt').reshape(-1, 3)
+        self.res = load_data(src, 'local-decoding.txt', 'uint64')
 
     def test_local_decoding(self):
         dec = poishmm.local_decoding(self.lcxpt)
@@ -22,10 +22,10 @@ class TestLocalDecoding(unittest.TestCase):
 class TestGloblaDecoding(unittest.TestCase):
     def setUp(self):
         src = 'data/earthquakes/3s'
-        self.lcxpt = load_data(src, 'lcxpt').reshape(-1, 3)
-        self.lgamma = load_data(src, 'lgamma_').reshape(3, 3)
-        self.ldelta = load_data(src, 'ldelta_')
-        self.res = load_data(src, 'globald', 'uint64')
+        self.lcxpt = load_data(src, 'lcsp.txt').reshape(-1, 3)
+        self.lgamma = load_data(src, 'lgamma.txt').reshape(3, 3)
+        self.ldelta = load_data(src, 'ldelta.txt')
+        self.res = load_data(src, 'global-decoding.txt', 'uint64')
 
     def test_global_decoding(self):
         dec = poishmm.global_decoding(self.lgamma, self.ldelta, self.lcxpt)
