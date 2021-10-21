28e19207001810028638d2166377ec06772558a3
Michael Blaß <mblass@posteo.net>
2021-10-21 14:24:37 +0200

    Update test utilities

--- tests/utils.py
+++ tests/utils.py
@@ -11,9 +11,9 @@ def gen_init_params(m_states: int, data: np.ndarray) -> tuple:
     """
     Generate initila parameters for HMM training.
     """
-    init_lambda = utils.StateDependentMeansInitializer.hist(data, m_states)
-    init_gamma = utils.TpmInitializer.softmax(m_states)
-    init_delta = utils.StartDistributionInitializer.stationary(init_gamma)
+    init_lambda = gen_sdm(data, m_states)
+    init_gamma = gen_prob_mat(m_states, m_states)
+    init_delta = gen_prob_mat(1, m_states)
     return init_lambda, init_gamma, init_delta
 
 
@@ -25,3 +25,18 @@ def load_data(path: Union[str, Path], file: Union[str, Path],
     path = Path(path)
     dfp = path.joinpath(file)
     return np.fromfile(str(dfp), dtype=dtype, sep='\n')
+
+
+def gen_sdm(data: np.ndarray, m_states: int) -> np.ndarray:
+    """
+    Generate an initial guess of the state-dependend means.
+    """
+    return np.linspace(data.min(), data.max(), m_states)
+
+
+def gen_prob_mat(rows: int, cols: int) -> np.ndarray:
+    """
+    Generate a random probability matrix.
+    """
+    var = np.exp(np.random.rand(rows, cols))
+    return var / var.sum(axis=1, keepdims=True)
