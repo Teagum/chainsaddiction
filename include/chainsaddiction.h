26037d0dda67574c3745206482a07dc4ab303cd1
Michael Blaß <mblass@posteo.net>
2023-07-02 21:20:23 +0200

    fix: unused parameters in gcc

--- include/chainsaddiction.h
+++ include/chainsaddiction.h
@@ -6,7 +6,7 @@
 #include "../src/chainsaddiction/restrict.h"
 
 
-#define UNUSED(obj) obj = NULL;
+#define UNUSED(obj) (void)(obj)
 
 #define PyArray_NEW_LD(py_obj) \
     (PyArrayObject *) PyArray_FROM_OTF (py_obj, NPY_LONGDOUBLE, NPY_ARRAY_IN_ARRAY);
