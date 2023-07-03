22d06fce2fa24a3a09725dceb114a2a6aed8f8ef
Michael Blaß <mblass@posteo.net>
2023-07-03 11:52:27 +0200

    refactor: update type hints
    - Rename class
    - add type hints for members

--- src/chainsaddiction/poishmm/__init__.pyi
+++ src/chainsaddiction/poishmm/__init__.pyi
@@ -1,12 +1,24 @@
 from typing import Any
-from .. typing import AnyArray
+from .. typing import AnyArray, Float128Array
 
 
-class PoissonHmm:
-    ...
+class PoisHmm:
+    err: bool
+    n_iter: int
+    m_states: int
+    aic: float
+    bic: float
+    llk: float
+    lambda_: Float128Array
+    gamma_: Float128Array
+    delta_: Float128Array
+    lalpha: Float128Array
+    lbeta: Float128Array
+    lcsp: Float128Array
+
 
 def fit(n_obs: int, m_states: int, max_iter: int, tol: float, sdm: AnyArray,
-        tpm: AnyArray, distr: AnyArray, data: AnyArray) -> PoissonHmm:
+        tpm: AnyArray, distr: AnyArray, data: AnyArray) -> PoisHmm:
     ...
 
 def read_params(path: str) -> dict[str, Any]:
